15036a134110c12f91363889888744854cb77130
Developer <denbudeyko@yandex.ru>
2019-08-26 15:21:37 +0300

    3th lesson

---
+++ config/init.php
@@ -0,0 +1,24 @@
+<?php
+
+define("DEBUG",1);
+define("ROOT", dirname(__DIR__));
+define('WWW', ROOT .'/public');
+define('APP', ROOT .'/app');
+define('CORE', ROOT.'/vendor/core');
+define('LIBS', ROOT.'/vendor/libs');
+define('CACHE', ROOT.'/tmp/cache');
+define('CONFIG', ROOT.'/config');
+define('LAYOUT','default');
+
+
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+
+$app_path = preg_replace('#[^/]+$#','', $app_path);
+$app_path = str_replace('/public/', '', $app_path);
+
+
+define('PATH', $app_path);
+define('ADMIN', PATH.'/admin');
+
+
+require_once ROOT .'/vendor/autoload.php';
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once __DIR__ .'/../config/init.php';
+ echo 'Тестируем перенаправления на Front Controller';
+?>
